2510d885775fab5d8a2b0ddbd59f12fe2bd4635c
sanjay <sanjaykumar73189@gmail.com>
2023-02-05 14:23:41 +0530

    Add Membership

--- src/components/AdminControl/Books/AddMembership.js
+++ src/components/AdminControl/Books/AddMembership.js
@@ -1,10 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AdminMembershipCard from "./AdminMembershipCard";
+import { useNavigation } from "@react-navigation/native";
 
 const AddMembership = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>AddMembership</Text>
+    <View
+      style={{
+        backgroundColor: "#F6E1DC",
+        minHeight: "100%",
+        width: "100%",
+
+        // alignItems: "center",
+      }}
+    >
+      <SafeAreaView style={{ marginTop: 10 }}>
+        <Text style={{ marginTop: 20, fontSize: 20, alignSelf: "center" }}>
+          Membership
+        </Text>
+        <View style={{ alignItems: "center", marginTop: 20 }}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("adminmembership")}
+          >
+            <Text
+              style={{
+                backgroundColor: "#3d3a4bff",
+                color: "white",
+                paddingHorizontal: 20,
+                paddingVertical: 10,
+                fontSize: 18,
+                borderBottomRightRadius: 20,
+                borderTopLeftRadius: 20,
+              }}
+            >
+              Add Membership
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <AdminMembershipCard />
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
--- src/components/AdminControl/Membership/AdminMembership.js
+++ src/components/AdminControl/Membership/AdminMembership.js
@@ -1,10 +1,110 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, TextInput } from "react-native";
 import React from "react";
 
 const AdminMembership = () => {
   return (
-    <View>
-      <Text>AdminMembership</Text>
+    <View
+      style={{
+        backgroundColor: "#F6E1DC",
+        minHeight: "100%",
+        width: "100%",
+        marginTop: 30,
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ marginBottom: 30, marginTop: 20, fontSize: 20 }}>
+        Add Membership
+      </Text>
+      <View style={{ marginBottom: 30 }}>
+        {/* <Text>File Picker Here</Text> */}
+      </View>
+      <View style={{ width: "80%", height: 50, marginBottom: 10 }}>
+        <TextInput
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            width: "100%",
+            height: "100%",
+            marginBottom: 10,
+            borderRadius: 20,
+            // marginTop: 20,
+            paddingLeft: 20,
+          }}
+          //   onChangeText={}
+          //   value={number}
+          placeholder="Membership Name"
+          //   keyboardType="numeric"
+        />
+      </View>
+      <View style={{ width: "80%", height: 50, marginBottom: 10 }}>
+        <TextInput
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            width: "100%",
+            height: "100%",
+            borderRadius: 20,
+            // marginTop: 20,
+            paddingLeft: 20,
+          }}
+          //   onChangeText={}
+          //   value={number}
+          placeholder="Description"
+          //   keyboardType="numeric"
+        />
+      </View>
+      <View style={{ width: "80%", height: 50, marginBottom: 10 }}>
+        <TextInput
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            width: "100%",
+            height: "100%",
+            marginBottom: 10,
+            borderRadius: 20,
+            // marginTop: 20,
+            paddingLeft: 20,
+          }}
+          //   onChangeText={}
+          //   value={number}
+          placeholder="Price"
+          //   keyboardType="numeric"
+        />
+      </View>
+      <View style={{ width: "80%", height: 50, marginBottom: 10 }}>
+        <TextInput
+          style={{
+            borderColor: "black",
+            borderWidth: 1,
+            width: "100%",
+            height: "100%",
+            marginBottom: 10,
+            borderRadius: 20,
+            // marginTop: 20,
+            paddingLeft: 20,
+          }}
+          //   onChangeText={}
+          //   value={number}
+          placeholder="Duration in Days"
+          keyboardType="numeric"
+        />
+      </View>
+
+      <TouchableOpacity>
+        <View style={{ marginTop: 10 }}>
+          <Text
+            style={{
+              paddingHorizontal: 50,
+              paddingVertical: 15,
+              borderRadius: 10,
+              backgroundColor: "#3d3a4bff",
+              color: "white",
+            }}
+          >
+            Upload Membership
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
